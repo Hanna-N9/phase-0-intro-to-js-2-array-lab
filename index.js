c571d22ad17bf4dc8af38e2dfeb9f755af6d16ea
Hanna Negash <hnegash.n@gmail.com>
2023-08-04 19:08:28 -0700

    manipulate arrays in index.js file

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+console.log(cats);
+
+// appends a cat to the end of the cats array - destructive
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+//prepends a cat to the beginning of the cats array - destructive
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+//removes the last cat from the cats array - destructive
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+//removes the first cat from the cats array - destructive
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// appends a cat to the end of the cats array - nondestructive
+function appendCat(name) {
+  const copyCats = [...cats];
+  copyCats.push(name);
+  return copyCats;
+}
+
+// prepends a cat to the cats array and returns a new array, leaving the cats array unchanged - nondestructive
+function prependCat(name) {
+  const copyCats = [...cats];
+  copyCats.unshift(name);
+  return copyCats;
+}
+
+// removes the last cat in the cats array and returns a new array, leaving the cats array unchanged - nondestructive
+function removeLastCat() {
+  const copyCats = [...cats];
+  copyCats.pop();
+  return copyCats;
+}
+
+// removes the first cat from the cats array and returns a new array, leaving the cats array unchanged - nondestructive
+function removeFirstCat() {
+  const copyCats = [...cats];
+  copyCats.shift();
+  return copyCats;
+}
